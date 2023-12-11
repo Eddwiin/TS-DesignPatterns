76988a5e4c41d2315487e74e6d8902d3039f4ca3
Eddwiin <maguran.mahendrarajan@gmail.com>
2023-12-11 23:44:55 +0100

    create person builder

---
+++ src/app/person/PersonBuilder/PersonBuilder.ts
@@ -0,0 +1,56 @@
+export class Person {
+    username = '';
+    password = '';
+    name = '';
+    email = '';
+    age = 0;
+    
+    static get new() {
+        return new Builder();
+    }
+}
+
+
+export class PersonBuilder {
+    protected person = new Person();
+
+    build() {
+        return this.person
+    }
+}
+
+export class PersonInfoBuilder<SELF extends PersonInfoBuilder<SELF>> extends PersonBuilder {
+    setUsername(username: string) {
+        this.person.username = username;
+        return this;
+    }
+
+    setPassword(password: string) {
+        this.person.password = password;
+        return this;
+    }
+}
+
+export class PersonWithNameAndEmail<SELF extends PersonWithNameAndEmail<SELF>> extends PersonInfoBuilder<PersonWithNameAndEmail<SELF>> {
+    setName(name: string) {
+        this.person.name = name;
+        return this;
+    }
+
+    setEmail(email: string) {
+        this.person.email = email;
+        return this;
+    }
+}
+
+export class PersonWithAge<SELF extends PersonWithAge<SELF>> extends PersonWithNameAndEmail<PersonWithAge<SELF>> {
+    setAge(age: number) {
+        this.person.age = age;
+        return this;
+    }
+}
+
+
+export class Builder extends PersonWithAge<Builder> {
+
+}
---
+++ src/app/person/person.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Person, PersonBuilder, PersonInfoBuilder } from './PersonBuilder/PersonBuilder';
+
+@Component({
+  selector: 'app-person',
+  standalone: true,
+  imports: [],
+  templateUrl: './person.component.html',
+  styleUrl: './person.component.scss'
+})
+export class PersonComponent implements OnInit {
+
+  ngOnInit() {
+    this.loadPersonBuilder();
+  }
+
+  loadPersonBuilder() {
+    const person = Person.new.setName('MyTest').setEmail('Email').setPassword('azerty').setUsername('edouard').setAge(12);
+
+    console.log(person);
+  }
+}
