bb618611e730fa1ead1a3f83dc2d9a3db0b5f263
Maguran MAHENDRARAJAN <maguran.mahendrarajan@gmail.com>
2024-02-10 23:17:41 +0100

    Add typed Array

--- src/app/Optional/optional.ts
+++ src/app/Optional/optional.ts
@@ -3,3 +3,18 @@ type Params = { id: string } & (
   {gender: 'Male' | 'Female', salary: number} | 
   { race: 'Dog' | 'Cat', age: number}
 )
+
+
+
+const typedObjectKeys = <TObj extends {}>(
+  obj: TObj
+): Array<keyof TObj> => {
+  return Object.values(obj) as Array<keyof TObj>;
+}
+
+const result = typedObjectKeys({
+  name: 'John',
+  age: 30
+});
+
+console.log(result)
