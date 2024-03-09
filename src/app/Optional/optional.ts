b7adab7a667785d64b2da9e8950272a324b8a77f
Maguran MAHENDRARAJAN <maguran.mahendrarajan@gmail.com>
2024-03-09 02:32:03 +0100

    Add OnlySubscriby example

--- src/app/Optional/optional.ts
+++ src/app/Optional/optional.ts
@@ -1,6 +1,6 @@
 
 type Params = { id: string } & (
-  {gender: 'Male' | 'Female', salary: number} | 
+  {gender: 'Male' | 'Female', salary: number} |
   { race: 'Dog' | 'Cat', age: number}
 )
 
@@ -18,3 +18,37 @@ const result = typedObjectKeys({
 });
 
 console.log(result)
+
+
+interface Person {
+  name: string;
+  age: number;
+  address: string;
+}
+
+interface Employee {
+  salary: number;
+  companyName: string;
+}
+
+type Getter<Type> = {
+  [Property in keyof Type as `get${Capitalize<string & Property>}`]: () => Type[Property]
+}
+
+type Solution = Getter<Person>;
+
+
+interface People {
+  subscribed_james: Person;
+  subscribed_kate: Person;
+  subscribed_todd: Person;
+  subscribed_tom: Person;
+  chris: Person;
+  timmy: Person;
+}
+
+type OnlySubscribed<Type> = {
+  [Property in keyof Type as Property extends `subscribed${infer _}` ? Property : never]: Type[Property]
+}
+
+type CoolPeople = OnlySubscribed<People>;
