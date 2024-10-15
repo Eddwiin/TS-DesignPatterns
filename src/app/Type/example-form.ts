b5af3630d5b3bdd58145df9b086c3f6c5a547acf
Maguran <maguran.mahendrarajan@gmail.com>
2024-10-15 17:12:54 +0200

    Add extract property from complex object with generic

---
+++ src/app/Type/example-form.ts
@@ -0,0 +1,12 @@
+interface ExampleForm {
+    firstName: string;
+    lastName: string;
+    contact: {
+        email: string;
+        phone: number;
+    }
+}
+
+type Path<T> = T extends object
+    ? { [key in keyof T]: [key] | [key, ...Path<T>]}[keyof T]
+    : never
